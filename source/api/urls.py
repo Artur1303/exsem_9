5e3dbfc2a813a618c3324c915c5804d42de710e5
Artur <metis130383@gmail.com>
2020-10-24 16:26:08 +0600

    Добаввлнны   view  в  Api и подключил  в urls

--- source/api/urls.py
+++ source/api/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.db import router
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from api.views import CreateView, DeleteView
 
 app_name = 'api'
 
+
 urlpatterns = [
+    path('favorites/<int:pk>/delete/', DeleteView.as_view(), name='favorites_delete'),
+    path('favorites/add/<int:pk>/', CreateView.as_view(), name='favorites_create')
+
 
 ]
--- source/api/views.py
+++ source/api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from webapp.models import Photo, Favorites
 
-# Create your views here.
+
+
+
+class CreateView(APIView,):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk=None):
+        photo = get_object_or_404(Photo, pk=pk)
+        created = Favorites.objects.get_or_create(photo=photo, author=request.user)
+        if created:
+            photo.save()
+            return Response({'pk': pk})
+        else:
+            return Response(status=403)
+
+
+class DeleteView(APIView):
+    permission_classes = [IsAuthenticated]
+    def delete(self, request, pk=None):
+        favorites = get_object_or_404(Favorites, pk=pk)
+        favorites.delete()
+        return Response({'pk': pk})
+
