2cea190195907b76d0b4aff3fcecd129f8123f37
Artur <metis130383@gmail.com>
2020-10-24 17:24:09 +0600

    Добавленна добовление и  удаление  в избранное

--- source/api/views.py
+++ source/api/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import ensure_csrf_cookie
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -7,10 +9,12 @@ from webapp.models import Photo, Favorites
 
 
 
-class CreateView(APIView,):
+class CreateView(APIView):
     permission_classes = [IsAuthenticated]
 
+    @method_decorator(ensure_csrf_cookie)
     def post(self, request, pk=None):
+        print(pk)
         photo = get_object_or_404(Photo, pk=pk)
         created = Favorites.objects.get_or_create(photo=photo, author=request.user)
         if created:
@@ -22,8 +26,10 @@ class CreateView(APIView,):
 
 class DeleteView(APIView):
     permission_classes = [IsAuthenticated]
+
+    @method_decorator(ensure_csrf_cookie)
     def delete(self, request, pk=None):
-        favorites = get_object_or_404(Favorites, pk=pk)
+        favorites = get_object_or_404(Favorites, photo_id=pk)
         favorites.delete()
         return Response({'pk': pk})
 
